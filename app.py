47ea6743c88dba2482a1e4f718392aab7657fc2d
Jeff Langballe <jeffreylangballe@protonmail.com>
2021-12-02 12:02:39 -0500

    Add colour set endpoint

--- app.py
+++ app.py
@@ -6,22 +6,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
-r = 255
-g = 180
-b = 100
+R = 255
+G = 180
+B = 100
+BRIGHTNESS = 0.15
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def refresh_tree():
+    pixels = neopixel.NeoPixel(board.D21, 100, brightness=BRIGHTNESS)
+    pixels.fill((0,0,0))
+    sleep(1)
+    pixels.fill((G,R,B))
+    pixels.show()
 
 @app.route('/brightness/<float:brightness>')
 def set_brightness(brightness):
     if 0 <= brightness <= 1:
-        pixels = neopixel.NeoPixel(board.D21, 100, brightness=brightness)
-        pixels.fill((0,0,0))
-        sleep (2)
-        pixels.fill((g,r,b))
-        pixels.show()
+        global BRIGHTNESS
+        BRIGHTNESS = brightness
+        refresh_tree()
         return 'ok'
     else:
         return 'brightness not in [0, 1]', 400
+
+@app.route('/colour/<int:r>/<int:g>/<int:b>')
+def set_colour(r, g, b):
+    if 0 <= r <= 255 and 0 <= g <=255 and 0 <= b <= 255:
+        global R, G, B
+        R, G, B = r, g, b
+        refresh_tree()
+        return 'ok'
+    else:
+        return 'r,b,g not in range [0, 255]', 400
